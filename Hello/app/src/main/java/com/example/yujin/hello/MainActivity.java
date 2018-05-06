b832aa42a7db38a205358e88ec0f2d9011169a36
u0jin <yujin5836@gmail.com>
2018-05-06 22:24:41 +0900

    test

---
+++ Hello/app/src/main/java/com/example/yujin/hello/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.yujin.hello;
+// 이벤트 처리
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void btn1Click(View v){
+        Intent myintent=new Intent(getApplicationContext(),NewActivity.class);
+        startActivity(myintent);
+    }
+}
---
+++ Hello/app/src/main/java/com/example/yujin/hello/NewActivity.java
@@ -0,0 +1,22 @@
+package com.example.yujin.hello;
+// 이벤트 처리
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class NewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+    public void backbtnClick(View v){
+        Toast.makeText(getApplicationContext(),"되돌아가기 버튼이 눌렸습니다.",Toast.LENGTH_LONG);
+        finish(); //이전 화면 돌아가기
+    }
+
+}
