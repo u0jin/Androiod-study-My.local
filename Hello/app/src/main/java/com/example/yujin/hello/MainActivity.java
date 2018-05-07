1f77afbad76e3eb91db44010a478f7e0aae47001
u0jin <yujin5836@gmail.com>
2018-05-07 19:20:10 +0900

    test-editbtn
    edit btn 추가
    버튼 리스너 추가

--- Hello/app/src/main/java/com/example/yujin/hello/MainActivity.java
+++ Hello/app/src/main/java/com/example/yujin/hello/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText =(EditText)findViewById(R.id.editText1);
+        String strText =editText.getText().toString();
+
+        final EditText editText1=(EditText)findViewById(R.id.editText1);
+        Button button=(Button)findViewById(R.id.insetbutton);
+        final TextView textView=(TextView)findViewById(R.id.textview4);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(editText1.getText());
+            }
+        });
+
     }
     public void btn1Click(View v){
         Intent myIntent=new Intent(getApplicationContext(),NewActivity.class);
