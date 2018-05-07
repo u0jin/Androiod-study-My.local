abba3e2630cfe2c877627b522d55d3ee037c80c7
u0jin <yujin5836@gmail.com>
2018-05-08 02:06:58 +0900

    layout 설정

---
+++ Hanium_chart/app/src/main/java/com/example/yujin/hanium_chart/Breakfast.java
@@ -0,0 +1,17 @@
+package com.example.yujin.hanium_chart;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class Breakfast extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.breakfast);
+    }
+
+    }
+
